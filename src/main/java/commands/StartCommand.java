ef4ac724a4d5783ca10c88992887e0797e90fa09
DzirDev <zerqzmc@gmail.com>
2022-01-05 14:02:10 -0500

    Added some basics

---
+++ src/main/java/commands/StartCommand.java
@@ -0,0 +1,26 @@
+package commands;
+
+import main.Main;
+import managers.GameManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import utils.ColourUtils;
+
+public class StartCommand implements CommandExecutor {
+
+    private Main m;
+    private GameManager gm;
+    public StartCommand(Main m, GameManager gm){
+        this.m = m;
+        this.gm = gm;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
+        p.sendMessage(ColourUtils.colour("&4&lADMIN &8| &aAn administrator command was executed."));
+        return true;
+    }
+}
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,14 @@
 package main;
 
+import events.JoinListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
-
+        Bukkit.getPluginManager().registerEvents(new JoinListener(this), this);
     }
 
     @Override
