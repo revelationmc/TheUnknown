c4b123373bb6b27f809878c0a0d41a7ad4c04201
DzirDev <zerqzmc@gmail.com>
2022-01-15 11:57:24 -0500

    a

--- src/main/java/managers/SpectatorManager.java
+++ src/main/java/managers/SpectatorManager.java
@@ -1,5 +1,7 @@
 package managers;
 
+import org.bukkit.entity.Player;
+
 import main.Main;
 
 public class SpectatorManager {
@@ -10,6 +12,10 @@ public class SpectatorManager {
 	public SpectatorManager(Main main) {
 		this.main = main;
 		this.gm = new GameManager(main);
+	}
+	
+	public void checkIfSpec(Player p) {
+		
 	}
 	
 }
