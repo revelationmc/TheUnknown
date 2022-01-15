f9e7347a1c6e46a3e39e5b403bde49a093699a2e
DzirDev <zerqzmc@gmail.com>
2022-01-15 11:55:14 -0500

    e

--- src/main/java/managers/SpectatorManager.java
+++ src/main/java/managers/SpectatorManager.java
@@ -1,5 +1,15 @@
 package managers;
 
+import main.Main;
+
 public class SpectatorManager {
 
+	private Main main;
+	private GameManager gm;
+	
+	public SpectatorManager(Main main) {
+		this.main = main;
+		this.gm = new GameManager(main);
+	}
+	
 }
